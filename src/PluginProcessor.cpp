f92a9ec7a7e8f48929dcd039778f88500fe0cb30
Otto Ahoniemi <otto@ottoahoniemi.fi>
2025-06-18 21:44:28 +0300

    Add basic controls

--- src/PluginProcessor.cpp
+++ src/PluginProcessor.cpp
@@ -1,182 +1,69 @@
 #include "PluginProcessor.h"
-#include "LevelDetector.h"
 #include "PluginEditor.h"
-#include "Utils.h"
 #include "juce_audio_basics/juce_audio_basics.h"
 #include <vector>
 
 static const std::vector<mrta::ParameterInfo> Parameters{
-    // gate
-    {Param::ID::GateEnabled, Param::Name::GateEnabled,
-     Param::Ranges::EnabledOff, Param::Ranges::EnabledOn,
-     Param::Defaults::GateEnabledDefault},
-    {Param::ID::GateOutputGain, Param::Name::GateOutputGain, Param::Units::Db,
-     Param::Defaults::GateOutputGainDefault, Param::Ranges::OutputGainMin,
-     Param::Ranges::OutputGainMax, Param::Ranges::OutputGainInc,
-     Param::Ranges::OutputGainSkw},
-    {Param::ID::GateThreshold, Param::Name::GateThreshold, Param::Units::Db,
-     Param::Defaults::GateThresholdDefault, Param::Ranges::GateThresholdMin,
-     Param::Ranges::GateThresholdMax, Param::Ranges::GateThresholdInc,
-     Param::Ranges::GateThresholdSkw},
-    {Param::ID::GateHysteresis, Param::Name::GateHysteresis, Param::Units::Db,
-     Param::Defaults::GateHysteresisDefault, Param::Ranges::GateHysteresisMin,
-     Param::Ranges::GateHysteresisMax, Param::Ranges::GateHysteresisInc,
-     Param::Ranges::GateHysteresisSkw},
-    {Param::ID::GateReduction, Param::Name::GateReduction, Param::Units::Db,
-     Param::Defaults::GateReductionDefault, Param::Ranges::GateReductionMin,
-     Param::Ranges::GateReductionMax, Param::Ranges::GateReductionInc,
-     Param::Ranges::GateReductionSkw},
-    {Param::ID::GateAttack, Param::Name::GateAttack, Param::Units::Ms,
-     Param::Defaults::GateAttackDefault, Param::Ranges::GateAttackMin,
-     Param::Ranges::GateAttackMax, Param::Ranges::GateAttackInc,
-     Param::Ranges::GateAttackSkw},
-    {Param::ID::GateHold, Param::Name::GateHold, Param::Units::Ms,
-     Param::Defaults::GateHoldDefault, Param::Ranges::GateHoldMin,
-     Param::Ranges::GateHoldMax, Param::Ranges::GateHoldInc,
-     Param::Ranges::GateHoldSkw},
-    {Param::ID::GateRelease, Param::Name::GateRelease, Param::Units::Ms,
-     Param::Defaults::GateReleaseDefault, Param::Ranges::GateReleaseMin,
-     Param::Ranges::GateReleaseMax, Param::Ranges::GateReleaseInc,
-     Param::Ranges::GateReleaseSkw},
-
-    // compressor
-    {Param::ID::CompressorEnabled, Param::Name::CompressorEnabled,
-     Param::Ranges::EnabledOff, Param::Ranges::EnabledOn,
-     Param::Defaults::CompressorEnabledDefault},
-    {Param::ID::CompressorOutputGain, Param::Name::CompressorOutputGain,
-     Param::Units::Db, Param::Defaults::CompressorOutputGainDefault,
-     Param::Ranges::OutputGainMin, Param::Ranges::OutputGainMax,
-     Param::Ranges::OutputGainInc, Param::Ranges::OutputGainSkw},
-    {Param::ID::CompressorThreshold, Param::Name::CompressorThreshold,
-     Param::Units::Db, Param::Defaults::CompressorThresholdDefault,
-     Param::Ranges::CompressorThresholdMin,
-     Param::Ranges::CompressorThresholdMax,
-     Param::Ranges::CompressorThresholdInc,
-     Param::Ranges::CompressorThresholdSkw},
-    {Param::ID::CompressorRatio, Param::Name::CompressorRatio, Param::Units::Db,
-     Param::Defaults::CompressorRatioDefault, Param::Ranges::CompressorRatioMin,
-     Param::Ranges::CompressorRatioMax, Param::Ranges::CompressorRatioInc,
-     Param::Ranges::CompressorRatioSkw},
-    {Param::ID::CompressorKnee, Param::Name::CompressorKnee, Param::Units::Db,
-     Param::Defaults::CompressorKneeDefault, Param::Ranges::CompressorKneeMin,
-     Param::Ranges::CompressorKneeMax, Param::Ranges::CompressorKneeInc,
-     Param::Ranges::CompressorKneeSkw},
-    {Param::ID::CompressorAttack, Param::Name::CompressorAttack,
-     Param::Units::Ms, Param::Defaults::CompressorAttackDefault,
-     Param::Ranges::CompressorAttackMin, Param::Ranges::CompressorAttackMax,
-     Param::Ranges::CompressorAttackInc, Param::Ranges::CompressorAttackSkw},
-    {Param::ID::CompressorRelease, Param::Name::CompressorRelease,
-     Param::Units::Ms, Param::Defaults::CompressorReleaseDefault,
-     Param::Ranges::CompressorReleaseMin, Param::Ranges::CompressorReleaseMax,
-     Param::Ranges::CompressorReleaseInc, Param::Ranges::CompressorReleaseSkw},
-    {Param::ID::CompressorMakeupGain, Param::Name::CompressorMakeupGain,
-     Param::Units::Db, Param::Defaults::CompressorMakeupGainDefault,
-     Param::Ranges::CompressorMakeupGainMin,
-     Param::Ranges::CompressorMakeupGainMax,
-     Param::Ranges::CompressorMakeupGainInc,
-     Param::Ranges::CompressorMakeupGainSkw},
-};
+    {
+        Param::ID::Enabled,
+        Param::Name::Enabled,
+        Param::Ranges::EnabledOff,
+        Param::Ranges::EnabledOn,
+        Param::Defaults::ProcessorEnabledDefault,
+    },
+    {
+        Param::ID::OutputGain,
+        Param::Name::OutputGain,
+        Param::Units::Db,
+        Param::Defaults::OutputGainDefault,
+        Param::Ranges::OutputGainMin,
+        Param::Ranges::OutputGainMax,
+        Param::Ranges::OutputGainInc,
+        Param::Ranges::OutputGainSkw,
+    },
+    {
+        Param::ID::FroginessLevel,
+        Param::Name::FroginessLevel,
+        Param::Units::Percentage,
+        Param::Defaults::FroginessDefault,
+        Param::Ranges::FroginessMin,
+        Param::Ranges::FroginessMax,
+        Param::Ranges::FroginessInc,
+        Param::Ranges::FroginessSkw,
+    }};
 
 DynamicsAudioProcessor::DynamicsAudioProcessor()
     : parameterManager(*this, ProjectInfo::projectName, Parameters) {
-  // gate
   parameterManager.registerParameterCallback(
-      Param::ID::GateEnabled, [this](float newValue, bool /*forced*/) {
-        gateEnabled = (newValue > 0.5f);
+      Param::ID::Enabled, [this](float newValue, bool /*forced*/) {
+        processorEnabled = (newValue > 0.5f);
       });
   parameterManager.registerParameterCallback(
-      Param::ID::GateOutputGain, [this](float value, bool forced) {
-        float gainLinear = juce::Decibels::decibelsToGain(value);
+      Param::ID::OutputGain, [this](float newValue, bool forced) {
+        float gainLinear = juce::Decibels::decibelsToGain(newValue);
         if (forced) {
-          gateOutputGainSmoother.setCurrentAndTargetValue(gainLinear);
+          outputGainSmoother.setCurrentAndTargetValue(gainLinear);
         } else {
-          gateOutputGainSmoother.setTargetValue(gainLinear);
+          outputGainSmoother.setTargetValue(gainLinear);
         }
       });
   parameterManager.registerParameterCallback(
-      Param::ID::GateThreshold, [this](float newValueDb, bool /*forced*/) {
-        gateOpenThresholdDb = newValueDb;
-        gateCloseThresholdDb = gateOpenThresholdDb + gateHysteresisDb;
-      });
-  parameterManager.registerParameterCallback(
-      Param::ID::GateHysteresis, [this](float newValueDb, bool /*forced*/) {
-        gateHysteresisDb = newValueDb;
-        gateCloseThresholdDb = gateOpenThresholdDb + gateHysteresisDb;
-      });
-  parameterManager.registerParameterCallback(
-      Param::ID::GateReduction, [this](float newValueDb, bool /*forced*/) {
-        gateReductionDb = newValueDb;
-      });
-  parameterManager.registerParameterCallback(
-      Param::ID::GateAttack, [this](float newValueMs, bool /*forced*/) {
-        gateAttackCoefficient =
-            Utils::calculateSmoothingCoefficient(newValueMs, currentSampleRate);
-      });
-  parameterManager.registerParameterCallback(
-      Param::ID::GateHold, [this](float newValueMs, bool /*forced*/) {
-        gateHoldSamples = Utils::msToSamples(newValueMs, currentSampleRate);
-      });
-  parameterManager.registerParameterCallback(
-      Param::ID::GateRelease, [this](float newValueMs, bool /*forced*/) {
-        gateReleaseCoefficient =
-            Utils::calculateSmoothingCoefficient(newValueMs, currentSampleRate);
-      });
-  // compressor
-  parameterManager.registerParameterCallback(
-      Param::ID::CompressorEnabled, [this](float newValue, bool /*forced*/) {
-        compressorEnabled = (newValue > 0.5f);
-      });
-  parameterManager.registerParameterCallback(
-      Param::ID::CompressorOutputGain, [this](float value, bool forced) {
-        float gainLinear = juce::Decibels::decibelsToGain(value);
+      Param::ID::FroginessLevel, [this](float newValue, bool forced) {
         if (forced) {
-          compressorOutputGainSmoother.setCurrentAndTargetValue(gainLinear);
+          froginessSmoother.setCurrentAndTargetValue(newValue);
         } else {
-          compressorOutputGainSmoother.setTargetValue(gainLinear);
+          froginessSmoother.setTargetValue(newValue);
         }
       });
-  parameterManager.registerParameterCallback(
-      Param::ID::CompressorThreshold,
-      [this](float value, bool /*forced*/) { compressorThresholdDb = value; });
-  parameterManager.registerParameterCallback(
-      Param::ID::CompressorRatio,
-      [this](float value, bool /*forced*/) { compressorRatio = value; });
-  parameterManager.registerParameterCallback(
-      Param::ID::CompressorKnee,
-      [this](float value, bool /*forced*/) { compressorKneeDb = value; });
-  parameterManager.registerParameterCallback(
-      Param::ID::CompressorAttack, [this](float newValueMs, bool /*forced*/) {
-        compressorAttackCoef =
-            Utils::calculateSmoothingCoefficient(newValueMs, currentSampleRate);
-      });
-  parameterManager.registerParameterCallback(
-      Param::ID::CompressorRelease, [this](float newValueMs, bool /*forced*/) {
-        compressorReleaseCoef =
-            Utils::calculateSmoothingCoefficient(newValueMs, currentSampleRate);
-      });
-  parameterManager.registerParameterCallback(
-      Param::ID::CompressorMakeupGain,
-      [this](float value, bool /*forced*/) { compressorMakeupGainDb = value; });
 }
 
 DynamicsAudioProcessor::~DynamicsAudioProcessor() {}
 
-void DynamicsAudioProcessor::resetInternalGateValuesToDefaults() {
-  gateIsOpen = true;
-  gateCurrentGainDb = 0.0f;
-  gateHoldCounter = 0;
-}
-
 void DynamicsAudioProcessor::prepareToPlay(double sampleRate,
                                            int samplesPerBlock) {
   currentSampleRate = sampleRate;
-  int numChannels = getTotalNumInputChannels();
-  levelDetectors.resize(numChannels);
-  for (auto &levelDetector : levelDetectors)
-    levelDetector.prepare(sampleRate);
-  compressorCurrentGainDb = 0.0f;
-  parameterManager.updateParameters(true);
-  resetInternalGateValuesToDefaults();
+  // int numChannels = getTotalNumInputChannels();
+  // TODO:
 }
 
 void DynamicsAudioProcessor::processBlock(juce::AudioBuffer<float> &buffer,
@@ -184,107 +71,15 @@ void DynamicsAudioProcessor::processBlock(juce::AudioBuffer<float> &buffer,
   juce::ScopedNoDenormals noDenormals;
   parameterManager.updateParameters();
 
-  if (!gateEnabled && !compressorEnabled) {
+  if (!processorEnabled) {
     return;
   }
 
-  int numChannels = buffer.getNumChannels();
-  int numSamples = buffer.getNumSamples();
-
-  for (int sample = 0; sample < numSamples; ++sample) {
-    // --- LEVEL DETECTION ---//
-    float maxEnvelopeDb = -101.0f; // Start with a very low value, below
-                                   // silenceFloor from level detector
-    for (int channel = 0; channel < numChannels; ++channel) {
-      auto *channelData = buffer.getReadPointer(channel);
-      LevelDetector &levelDetector = levelDetectors[channel];
-      float inputSample = channelData[sample];
-      float envDb = levelDetector.process(inputSample);
-      if (envDb > maxEnvelopeDb)
-        maxEnvelopeDb = envDb;
-    }
-
-    //--- GATE LOGIC  ---//
-    float gateCurrentGainLinear = 1.0f;
-    if (gateEnabled) {
-      float gateEnvelopeDb = maxEnvelopeDb;
-      if (gateEnvelopeDb > gateOpenThresholdDb) {
-        gateIsOpen = true;
-        gateHoldCounter = static_cast<int>(gateHoldSamples);
-      } else if (gateEnvelopeDb < gateCloseThresholdDb) {
-        if (gateHoldCounter > 0) {
-          gateHoldCounter--;
-        } else {
-          gateIsOpen = false;
-        }
-      }
-      float gateTargetGainDb = gateIsOpen ? 0.0f : gateReductionDb;
-      if (gateTargetGainDb > gateCurrentGainDb) {
-        gateCurrentGainDb = Utils::calculateOnePoleSmoothedOutput(
-            gateCurrentGainDb, gateTargetGainDb, gateAttackCoefficient);
-      } else if (gateTargetGainDb < gateCurrentGainDb) {
-        gateCurrentGainDb = Utils::calculateOnePoleSmoothedOutput(
-            gateCurrentGainDb, gateTargetGainDb, gateReleaseCoefficient);
-      }
-      gateCurrentGainLinear =
-          juce::Decibels::decibelsToGain(gateCurrentGainDb) *
-          gateOutputGainSmoother.getNextValue();
-    }
-
-    //--- COMPRESSOR LOGIC ---//
-    float compressorCurrentGainLinear = 1.0f;
-    if (compressorEnabled) {
-      float inputLevelOverThresholdDb = maxEnvelopeDb - compressorThresholdDb;
-      float gainReductionDb = 0.0f;
-      // soft knee
-      if (compressorKneeDb > 0.0f) {
-        float kneeHalf = compressorKneeDb / 2.0f;
-        if (inputLevelOverThresholdDb <= -kneeHalf) {
-          gainReductionDb = 0.0f;
-        } else if (inputLevelOverThresholdDb > kneeHalf) {
-          gainReductionDb =
-              compressorThresholdDb + (inputLevelOverThresholdDb - kneeHalf) -
-              (compressorThresholdDb +
-               (inputLevelOverThresholdDb - kneeHalf) / compressorRatio);
-        } else {
-          gainReductionDb = ((1.0f / compressorRatio - 1.0f) *
-                             powf(maxEnvelopeDb - compressorThresholdDb +
-                                      compressorKneeDb / 2.0f,
-                                  2.0f)) /
-                            (2.0f * compressorKneeDb);
-        }
-      } else { // knee 0 -> hard knee
-        if (inputLevelOverThresholdDb > 0.0f) {
-          gainReductionDb = inputLevelOverThresholdDb -
-                            inputLevelOverThresholdDb / compressorRatio;
-        }
-      }
-
-      float compressorTargetGainDb = -gainReductionDb + compressorMakeupGainDb;
-      float compressorCoef = (compressorTargetGainDb > compressorCurrentGainDb)
-                                 ? compressorAttackCoef
-                                 : compressorReleaseCoef;
-      compressorCurrentGainDb = Utils::calculateOnePoleSmoothedOutput(
-          compressorCurrentGainDb, compressorTargetGainDb, compressorCoef);
-      compressorCurrentGainLinear =
-          juce::Decibels::decibelsToGain(compressorCurrentGainDb) *
-          compressorOutputGainSmoother.getNextValue();
-    }
-
-    //--- APPLY GAINS ---//
-    for (int channel = 0; channel < numChannels; ++channel) {
-      auto *writeBuffer = buffer.getWritePointer(channel);
-      auto *channelData = buffer.getReadPointer(channel);
-      writeBuffer[sample] = channelData[sample] * gateCurrentGainLinear *
-                            compressorCurrentGainLinear;
-    }
-  }
+  // TODO:
 }
 
 void DynamicsAudioProcessor::releaseResources() {
-  resetInternalGateValuesToDefaults();
-  for (auto &detector : levelDetectors)
-    detector.reset();
+  // TODO
 }
 
 void DynamicsAudioProcessor::getStateInformation(juce::MemoryBlock &destData) {
--- src/PluginProcessor.h
+++ src/PluginProcessor.h
@@ -1,147 +1,40 @@
 #pragma once
 
-#include "LevelDetector.h"
 #include <JuceHeader.h>
-#include <vector>
 
 namespace Param {
 namespace ID {
-// gate
-static const juce::String GateEnabled{"gate_enabled"};
-static const juce::String GateOutputGain{"gate_output_gain"};
-static const juce::String GateThreshold{"gate_threshold"};
-static const juce::String GateHysteresis{"gate_hysteresis"};
-static const juce::String GateReduction{"gate_reduction"};
-static const juce::String GateAttack{"gate_attack"};
-static const juce::String GateHold{"gate_hold"};
-static const juce::String GateRelease{"gate_release"};
-// compressor
-static const juce::String CompressorEnabled{"compressor_enabled"};
-static const juce::String CompressorOutputGain{"compressor_output_gain"};
-static const juce::String CompressorThreshold{"compressor_threshold"};
-static const juce::String CompressorRatio{"compressor_ratio"};
-static const juce::String CompressorKnee{"compressor_knee"};
-static const juce::String CompressorAttack{"compressor_attack"};
-static const juce::String CompressorRelease{"compressor_release"};
-static const juce::String CompressorMakeupGain{"compressor_makeup_gain"};
+static const juce::String Enabled{"enabled"};
+static const juce::String OutputGain{"output_gain"};
+static const juce::String FroginessLevel{"froginess_level"};
 } // namespace ID
 
 namespace Name {
-// gate
-static const juce::String GateEnabled{"Gate enabled"};
-static const juce::String GateOutputGain{"Gate output gain"};
-static const juce::String GateThreshold{"Gate threshold"};
-static const juce::String GateHysteresis{"Gate hysteresis"};
-static const juce::String GateReduction{"Gate reduction"};
-static const juce::String GateAttack{"Gate attack"};
-static const juce::String GateHold{"Gate hold"};
-static const juce::String GateRelease{"Gate release"};
-// compressor
-static const juce::String CompressorEnabled{"Compressor enabled"};
-static const juce::String CompressorOutputGain{"Compressor output gain"};
-static const juce::String CompressorThreshold{"Compressor threshold"};
-static const juce::String CompressorRatio{"Compressor ratio"};
-static const juce::String CompressorKnee{"Compressor knee"};
-static const juce::String CompressorAttack{"Compressor attack"};
-static const juce::String CompressorRelease{"Compressor release"};
-static const juce::String CompressorMakeupGain{"Compressor makeup gain"};
+static const juce::String Enabled{"Enabled"};
+static const juce::String OutputGain{"Output gain"};
+static const juce::String FroginessLevel{"Froginess level"};
 } // namespace Name
 
 namespace Ranges {
-// generic
 static const juce::String EnabledOff{"Off"};
 static const juce::String EnabledOn{"On"};
-static constexpr float OutputGainMin{-60.0f};
+static constexpr float OutputGainMin{-24.0f};
 static constexpr float OutputGainMax{6.0f};
 static constexpr float OutputGainInc{0.1f};
 static constexpr float OutputGainSkw{2.0f};
-
-// compressor
-static constexpr float CompressorThresholdMin{-50.0f};
-static constexpr float CompressorThresholdMax{0.0f};
-static constexpr float CompressorThresholdInc{0.1f};
-static constexpr float CompressorThresholdSkw{1.0f};
-
-static constexpr float CompressorRatioMin{1.0f};
-static constexpr float CompressorRatioMax{20.0f};
-static constexpr float CompressorRatioInc{0.1f};
-static constexpr float CompressorRatioSkw{1.0f};
-
-static constexpr float CompressorKneeMin{0.0f};
-static constexpr float CompressorKneeMax{24.0f};
-static constexpr float CompressorKneeInc{0.1f};
-static constexpr float CompressorKneeSkw{1.0f};
-
-static constexpr float CompressorAttackMin{0.0f};
-static constexpr float CompressorAttackMax{100.0f};
-static constexpr float CompressorAttackInc{0.1f};
-static constexpr float CompressorAttackSkw{1.0f};
-
-static constexpr float CompressorReleaseMin{0.0f};
-static constexpr float CompressorReleaseMax{500.0f};
-static constexpr float CompressorReleaseInc{0.1f};
-static constexpr float CompressorReleaseSkw{1.0f};
-
-static constexpr float CompressorMakeupGainMin{-24.0f};
-static constexpr float CompressorMakeupGainMax{24.0f};
-static constexpr float CompressorMakeupGainInc{0.1f};
-static constexpr float CompressorMakeupGainSkw{1.0f};
-
-// gate
-static constexpr float GateThresholdMin{-100.0f};
-static constexpr float GateThresholdMax{0.0f};
-static constexpr float GateThresholdInc{0.1f};
-static constexpr float GateThresholdSkw{1.0f};
-
-static constexpr float GateHysteresisMin{-20.0f};
-static constexpr float GateHysteresisMax{0.0f};
-static constexpr float GateHysteresisInc{0.1f};
-static constexpr float GateHysteresisSkw{1.0f};
-
-static constexpr float GateReductionMin{-100.0f};
-static constexpr float GateReductionMax{0.0f};
-static constexpr float GateReductionInc{0.1f};
-static constexpr float GateReductionSkw{1.0f};
-
-static constexpr float GateAttackMin{0.0f};
-static constexpr float GateAttackMax{100.0f};
-static constexpr float GateAttackInc{0.1f};
-static constexpr float GateAttackSkw{1.0f};
-
-static constexpr float GateHoldMin{0.0f};
-static constexpr float GateHoldMax{1000.f};
-static constexpr float GateHoldInc{0.1f};
-static constexpr float GateHoldSkw{1.0f};
-
-static constexpr float GateReleaseMin{0.0f};
-static constexpr float GateReleaseMax{10000.0f};
-static constexpr float GateReleaseInc{0.1f};
-static constexpr float GateReleaseSkw{0.2f};
+static constexpr float FroginessMin{0.0f};
+static constexpr float FroginessMax{100.0f};
+static constexpr float FroginessInc{1.0f};
+static constexpr float FroginessSkw{1.0f};
 } // namespace Ranges
 namespace Defaults {
-// gate
-static constexpr bool GateEnabledDefault{true};
-static constexpr float GateOutputGainDefault{0.0f};
-static constexpr float GateThresholdDefault{-50.0f};
-static constexpr float GateHysteresisDefault{-3.0f};
-static constexpr float GateReductionDefault{-100.0f};
-static constexpr float GateAttackDefault{3.0f};
-static constexpr float GateHoldDefault{40.0f};
-static constexpr float GateReleaseDefault{10.0f};
-// compressor
-static constexpr bool CompressorEnabledDefault{true};
-static constexpr float CompressorOutputGainDefault{0.0f};
-static constexpr float CompressorThresholdDefault{-18.0f};
-static constexpr float CompressorRatioDefault{4.0f};
-static constexpr float CompressorKneeDefault{6.0f};
-static constexpr float CompressorAttackDefault{5.0f};
-static constexpr float CompressorReleaseDefault{50.0f};
-static constexpr float CompressorMakeupGainDefault{0.0f};
+static constexpr bool ProcessorEnabledDefault{true};
+static constexpr float OutputGainDefault{0.0f};
+static constexpr float FroginessDefault{0.0f};
 } // namespace Defaults
 
 namespace Units {
-static const juce::String Ms{"ms"};
-static const juce::String Hz{"Hz"};
+static const juce::String Percentage{"%"};
 static const juce::String Db{"dB"};
 } // namespace Units
 } // namespace Param
@@ -173,43 +66,11 @@ public:
   void changeProgramName(int index, const juce::String &newName) override;
 
 private:
-  // generic
   mrta::ParameterManager parameterManager;
   double currentSampleRate = 0;
-
-  // gate params (stored directly)
-  bool gateEnabled = Param::Defaults::GateEnabledDefault;
-  juce::SmoothedValue<float> gateOutputGainSmoother;
-
-  float gateOpenThresholdDb = Param::Defaults::GateThresholdDefault;
-  float gateHysteresisDb = Param::Defaults::GateHysteresisDefault;
-  float gateCloseThresholdDb = gateOpenThresholdDb + gateHysteresisDb;
-  float gateReductionDb = Param::Defaults::GateReductionDefault;
-
-  // gate params (calculated in parameter callbacks)
-  float gateAttackCoefficient = 0.0f;
-  float gateHoldSamples = 0.0f;
-  float gateReleaseCoefficient = 0.0f;
-
-  // internal vals
-  std::vector<LevelDetector> levelDetectors;
-  bool gateIsOpen = true;
-  float gateCurrentGainDb = 0.0f;
-  int gateHoldCounter = 0;
-
-  // compressor params (user configurable)
-  bool compressorEnabled = Param::Defaults::CompressorEnabledDefault;
-  juce::SmoothedValue<float> compressorOutputGainSmoother;
-  float compressorThresholdDb = Param::Defaults::CompressorThresholdDefault;
-  float compressorRatio = Param::Defaults::CompressorRatioDefault;
-  float compressorKneeDb = Param::Defaults::CompressorKneeDefault;
-  float compressorMakeupGainDb = Param::Defaults::CompressorMakeupGainDefault;
-  float compressorAttackCoef = 0.0f;
-  float compressorReleaseCoef = 0.0f;
-  float compressorCurrentGainDb = 0.0f;
-
-  // helpers
-  void resetInternalGateValuesToDefaults();
+  bool processorEnabled = Param::Defaults::ProcessorEnabledDefault;
+  juce::SmoothedValue<float> outputGainSmoother;
+  juce::SmoothedValue<float> froginessSmoother;
 
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(DynamicsAudioProcessor)
 };
