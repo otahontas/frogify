2f8762e3aeaaa19b01acf365f363092a5950b4f4
Otto Ahoniemi <otto@ottoahoniemi.fi>
2025-06-18 22:12:26 +0300

    Remove utils

--- src/Utils.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace Utils {
-float msToSamples(float timeMs, double sampleRate);
-float calculateSmoothingCoefficient(float ms, double sampleRate);
-float calculateOnePoleSmoothedOutput(float currentValue, float targetValue,
-                                     float coefficient);
-} // namespace Utils
